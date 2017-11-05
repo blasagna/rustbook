2ddb2743e1c0429897fc0483588d5b492bac034c
blasagna <bdimaiolo@gmail.com>
2017-11-05 18:57:24 -0500

    directions

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,40 +5,40 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    const MAX: u32 = 100;
+  const MAX: u32 = 100;
 
-    // generate random number
-    let secret = rand::thread_rng().gen_range(1, MAX + 1);
+  // generate random number
+  let secret = rand::thread_rng().gen_range(1, MAX + 1);
 
-    println!("Guess the number!");
+  println!("Guess the number! It is a positive integer in [{}, {}]", 1, MAX);
 
-    // accept user input
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect(
-            "Failed to read input",
+  // accept user input
+  loop {
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect(
+        "Failed to read input",
         );
 
-        // attempt to parse numeric inputs
-        let guess: u32 = match guess.trim().parse() {
-            Ok(n) => n,
-            Err(_) => {
-                println!("Please enter an integer number");
-                continue;
-            }
-        };
+    // attempt to parse numeric inputs
+    let guess: u32 = match guess.trim().parse() {
+      Ok(n) => n,
+        Err(_) => {
+          println!("Please enter a postive integer number");
+          continue;
+        }
+    };
 
-        println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret) {
-            Ordering::Less => println!("Too small..."),
-            Ordering::Greater => println!("Too big..."),
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            }
-        };
-    }
+    match guess.cmp(&secret) {
+      Ordering::Less => println!("Too small..."),
+        Ordering::Greater => println!("Too big..."),
+        Ordering::Equal => {
+          println!("Correct!");
+          break;
+        }
+    };
+  }
 
-    println!("Goodbye.");
+  println!("Goodbye.");
 }
