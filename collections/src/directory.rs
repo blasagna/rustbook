f1c291217dbce3897799ad34b4f052de0fc205ad
Bob DiMaiolo <bob@cognescent.com>
2017-11-07 22:21:57 -0500

    more structs in company

--- collections/src/directory.rs
+++ collections/src/directory.rs
@@ -1,15 +1,68 @@
 use std::collections::HashMap;
 
 pub struct Company {
-name: String,
-        directory: HashMap<String, Vec<String>>,
+    name: String,
+    departments: HashMap<String, Department>,
+}
+
+pub struct Department {
+    name: String,
+    employees: Vec<String>,
+}
+
+//TODO: impl for employee, contractor, pointers needed?
+//TODO: make department adds generic across employee/contractor/intern
+pub struct Employee {
+    name: String,
+    ssn: String,
+    title: String,
+    location: String,
+    age: u8,
+    gender: String,
+    manager: Employee,
+    start_date: String,
+}
+
+pub struct Contractor {
+    name: String,
+    employer: Company,
+    manager: Employee,
+    title: String,
+    location: String,
+}
+
+pub struct Intern {
+    name: String,
+    manager: Employee,
+    title: String,
+    location: String,
+    end_date: String,
+}
+
+impl Department {
+    pub fn new(name: &str) -> Department {
+        Department {
+            name: name.to_string(),
+            employees: Vec::new(),
+        }
+    }
+
+    pub fn add_employee(&mut self, name: &str) {
+        self.employees.push(name);
+    }
+
+    pub fn add_employees(&mut self, names: Vec<&str>) {
+        for name in names {
+            self.add_employee(name);
+        }
+    }
 }
 
 // TODO: add tests
 impl Company {
   pub fn new(name: &str) -> Company {
     Company {
-name: name.to_string(),
+        name: name.to_string(),
         directory: HashMap::new()
     }
   }
