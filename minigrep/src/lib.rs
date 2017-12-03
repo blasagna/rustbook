501a02f3d032a5a81ae15158295d93f95fb6b15e
blasagna <bdimaiolo@gmail.com>
2017-12-03 15:23:40 -0500

    search func and some unit tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -3,40 +3,87 @@ use std::io::prelude::*;
 use std::error::Error;
 
 pub struct Config {
-query: String,
-         filename: String,
+    query: String,
+    filename: String,
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
     }
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    pub fn get_query(&self) -> &str {
+        &self.query
+    }
 
-    Ok(Config { query, filename })
-  }
-
-  pub fn get_query(&self) -> &str {
-    &self.query
-  }
-
-  pub fn get_filename(&self) -> &str {
-    &self.filename
-  }
+    pub fn get_filename(&self) -> &str {
+        &self.filename
+    }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>>{
-  let mut f = File::open(config.filename)?;
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
 
-  let mut contents = String::new();
-  f.read_to_string(&mut contents)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
 
-  println!("with text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
-  Ok(())
+    Ok(())
 }
 
-// FIXME: add tests
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "foo";
+        let contents = "some
+more
+words
+but one line with foo";
+
+        assert_eq!(vec!["but one line with foo"], search(query, contents));
+    }
+
+    #[test]
+    fn no_results() {
+        let query = "foo";
+        let contents = "no
+matches";
+
+        let results = search(query, contents);
+        assert!(results.is_empty());
+    }
+
+    #[test]
+    fn multiple_results() {
+        let query = "foo";
+        let contents = "no foo
+bar
+matches foo";
+
+        assert_eq!(vec!["no foo", "matches foo"], search(query, contents));
+    }
+}
