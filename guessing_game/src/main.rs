9b86b5b3f6e4a879a8f00d777685f4ae499263b7
Bob DiMaiolo <bdimaiolo@gmail.com>
2024-12-29 11:06:32 -0500

    update guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,43 +1,35 @@
-extern crate rand;
-
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-
-mod guess;
+use std::io;
 
 fn main() {
+    println!("Guess the number!");
 
-  // generate random number
-  let secret = rand::thread_rng().gen_range(1, guess::MAX + 1);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-  println!("Guess the number! It is a positive integer in [{}, {}]", 1, guess::MAX);
+    loop {
+        println!("Please input your guess.");
 
-  // accept user input
-  loop {
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Error parsing input");
+        let mut guess = String::new();
 
-    // attempt to parse numeric inputs
-    let guess = match guess.trim().parse() {
-      Ok(n) => guess::Guess::new(n),
-        Err(e) => {
-          println!("Error parsing input as numeric: {:?}", e);
-          continue;
-        },
-    };
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess.value());
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.value().cmp(&secret) {
-      Ordering::Less => println!("Too small..."),
-        Ordering::Greater => println!("Too big..."),
-        Ordering::Equal => {
-          println!("Correct!");
-          break;
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-    };
-  }
-
-  println!("Goodbye.");
+    }
 }
